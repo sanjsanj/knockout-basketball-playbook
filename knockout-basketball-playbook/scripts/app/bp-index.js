702d54906e746b4d416bd629446bddba8beb14e9
Sanjay Purswani <sanjsanj@hotmail.com>
2016-01-09 13:01:41 +0000

    Data array can be presented in the view

---
+++ knockout-basketball-playbook/scripts/app/bp-index.js
@@ -0,0 +1,28 @@
+﻿var data = [
+    { Id: 1, Name: 'Ball Handling' },
+    { Id: 2, Name: 'Passing' },
+    { Id: 3, Name: 'Shooting' },
+    { Id: 4, Name: 'Rebounding' },
+    { Id: 5, Name: 'Transition' },
+    { Id: 6, Name: 'Defense' },
+    { Id: 7, Name: 'Team Offense' },
+    { Id: 8, Name: 'Team Defense' }
+];
+
+var viewModel = {
+    tags: ko.observableArray(data),
+    tagToAdd: ko.observable(''),
+
+    addTag: function () {
+        this.tags.push({ Name: this.tagToAdd() });
+        this.tagToAdd('');
+    },
+
+    editTag: function () {
+    },
+
+    deleteTag: function () {
+    }
+};
+
+ko.applyBindings(viewModel);
